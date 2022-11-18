8a47874cad4f2f1cd6a02471126e171019408a7a
Jose Luis <jpm652@inlumine.ual.es>
2022-11-18 16:19:45 +0100

    Initial Commit

---
+++ Crystalite/GestionCrystalite/CrystaliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrystaliteApp.swift
+//  Crystalite
+//
+//  Created by Aula11 on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CrystaliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Principal()
+        }
+    }
+}
