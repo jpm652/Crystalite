55ea33ed3249f2d05bfc3b09e64d3b402069359f
Jose Luis <jpm652@inlumine.ual.es>
2022-11-18 17:48:35 +0100

    Vista registro creada

---
+++ Crystalite/GestionCrystalite/Registro.swift
@@ -0,0 +1,20 @@
+//
+//  Registro.swift
+//  Crystalite
+//
+//  Created by Aula11 on 18/11/22.
+//
+
+import SwiftUI
+
+struct Registro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Registro_Previews: PreviewProvider {
+    static var previews: some View {
+        Registro()
+    }
+}
