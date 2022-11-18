be3a15d92db64d81f31cf238e055633cf957674e
Jose Luis <jpm652@inlumine.ual.es>
2022-11-18 16:45:56 +0100

    Subida Entidades

---
+++ Crystalite/GestionCrystalite/Login.swift
@@ -0,0 +1,23 @@
+//
+//  Login.swift
+//  Crystalite
+//
+//  Created by Aula11 on 18/11/22.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
