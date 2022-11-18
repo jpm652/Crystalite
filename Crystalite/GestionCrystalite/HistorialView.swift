5ea02d54d73308b0c6314b7273b09c2cdda0a754
Jose Luis <jpm652@inlumine.ual.es>
2022-11-18 17:55:46 +0100

    historialView

---
+++ Crystalite/GestionCrystalite/HistorialView.swift
@@ -0,0 +1,20 @@
+//
+//  HistorialView.swift
+//  Crystalite
+//
+//  Created by Aula11 on 18/11/22.
+//
+
+import SwiftUI
+
+struct HistorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistorialView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistorialView()
+    }
+}
