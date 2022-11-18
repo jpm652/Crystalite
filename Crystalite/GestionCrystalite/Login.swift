cc62fc2af92f80fac2592ce3756018b0840407c3
Adrianrm00 <arm115@inlumine.ual.es>
2022-11-18 17:57:30 +0100

    Subida de login

--- Crystalite/GestionCrystalite/Login.swift
+++ Crystalite/GestionCrystalite/Login.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct Login: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        ZStack{ Color(red: 0.898, green: 0.918, blue: 0.961).ignoresSafeArea()
+            VStack {
+                Image("Logo").resizable().frame(width: 350, height: 350, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 1) {
+                    Text("Email")
+                    TextField("Introducir email", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/).frame(width: 272, height: 34).background(.white).cornerRadius(15)
+                }.frame(width: 272, height: 58, alignment: .center)
+                VStack(alignment: .leading, spacing: 1) {
+                    Text("Contraseña")
+                    TextField("Introducir contraseña", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                }.frame(width: 272, height: 58, alignment: .center)
+                
+                
+                Button("Iniciar sesion") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Divider()
+                Button("Registrarse") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
         }
     }
 }
